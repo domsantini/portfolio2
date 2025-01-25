4f8cc908cb7360ec4dc61bf320626356e7c522e3
domsantini <111545540+domsantini@users.noreply.github.com>
2025-01-24 17:30:24 -0800

    Testing

--- app/components/MainContentWrapper.jsx
+++ app/components/MainContentWrapper.jsx
@@ -6,7 +6,7 @@ function MainContentWrapper({ children }) {
   // const borderRadius = useTransform(scrollYProgress, [0,1], [40,0])
     
   return (
-    <motion.section className='sticky top-0 bg-zinc-700 h-full'>
+    <motion.section className='sticky top-0 bg-zinc-700 h-full '>
       {children}
     </motion.section>
   )
