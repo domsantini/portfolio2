790492dc340a6581ee42527fd653aa3aa80f41ff
domsantini <111545540+domsantini@users.noreply.github.com>
2025-02-13 14:54:01 -0800

    actually fix section height

--- app/components/WorkSection.jsx
+++ app/components/WorkSection.jsx
@@ -9,7 +9,7 @@ const WorkSection = forwardRef(function WorkSection({ id }, ref ) {
       ref={ref} 
       id={id}
       className='
-        relative z-[2] min-h-[100svh] h-full p-4 md:px-8 lg:px-20
+        relative z-[2] min-h-[100svh] h-screen p-4 md:px-8 lg:px-20
         pt-20
         flex flex-col
         text-[#E6E8E3] bg-zinc-700
@@ -18,7 +18,7 @@ const WorkSection = forwardRef(function WorkSection({ id }, ref ) {
       <h1 className='text-[clamp(45px,9vw,140px)] font-chillax font-semibold pb-20'>
         Projects / Work
       </h1>
-      <div className='flex h-full text-sm sm:text-base pb-[calc(100dvh-100svh)]'>
+      <div className='flex h-full text-sm sm:text-base'>
         <ProjectsList />
       </div>
     </section>
