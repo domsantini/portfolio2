2eaf40444138a5d125523797fd7f8f041fe0bef8
domsantini <111545540+domsantini@users.noreply.github.com>
2025-03-19 12:22:16 -0700

    Refactor Footer

---
+++ app/components/Footer.jsx
@@ -0,0 +1,17 @@
+import ContactButton from "./ContactButton";
+
+function Footer() {
+  return (
+    <footer className='relative h-[100dvh] bg-[#FBFCF8]'>
+        <div className='h-full w-full fixed bottom-0 flex flex-col justify-center items-center'>
+          <p className='font-chillax font-semibold text-[12vw]'>Get in touch!</p>
+          <div className='flex  gap-2'>
+            <ContactButton label='LinkedIn' href='https://www.linkedin.com/in/dominic-santini/'/>
+            <ContactButton label='GitHub' href='https://github.com/domsantini'/>
+          </div>
+        </div>
+      </footer>
+  )
+}
+
+export default Footer;
--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ import Header from "./components/Header/Header";
 import LandingSection from "./components/LandingSection/LandingSection";
 import WorkSection from "./components/WorkSection";
 import AboutSection from "./components/AboutSection";
-import ContactButton from "./components/ContactButton";
+import Footer from "./components/Footer";
 
 export default function Home() {
   const wrapperRef = React.useRef(null)
@@ -31,32 +31,7 @@ export default function Home() {
           <AboutSection id="about"/>
         </div>
       </main>
-      <footer className='relative h-[100dvh] bg-[#FBFCF8]'>
-        <div className='h-full w-full fixed bottom-0 flex flex-col justify-center items-center'>
-          <p className='font-chillax font-semibold text-[12vw]'>Get in touch!</p>
-          <div className='flex  gap-2'>
-            <ContactButton label='LinkedIn' href='https://www.linkedin.com/in/dominic-santini/'/>
-            <ContactButton label='GitHub' href='https://github.com/domsantini'/>
-          </div>
-        </div>
-      </footer>
+      <Footer />
     </>
   );
 }
-
-{/* <div
-  ref={wrapperRef}
-  // style={{ borderRadius }}
-  className="relative z-[2] bg-zinc-700 h-full"
-> */}
-  {/* <ContactSection id='contact' className='sticky bottom-0 z-[3] bg-[#FBFCF8]'/> */}
-{/* </div> */}
-{/* <motion.div
-  ref={wrapperRef}
-  // style={{ borderRadius }}
-  className="relative z-[2] bg-zinc-700 h-[250svh] border-solid border-2 border-pink-400"
->
-  <WorkSection id="work" className='relative z-[5]'/>
-  <AboutSection id="about" className='relative z-[6]'/>
-  <ContactSection id='contact' className='sticky bottom-0 z-[3] bg-[#FBFCF8] border-2 border-solid border-green-400'/>
-</motion.div> */}
